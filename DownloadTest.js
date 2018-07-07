28396a549dccc38a65ec429f04b8ac02b35caae5
nidhinkumar <nidhinkumar@codingtown.com>
2018-07-07 16:02:22 +0530

    add excel downloading by creating seperate folder

--- DownloadTest.js
+++ DownloadTest.js
@@ -1,11 +1,17 @@
 /* @flow */
 
 import React, { Component } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from "react-native";
 import XLSX from "xlsx";
-import { writeFile, DocumentDirectoryPath } from "react-native-fs";
+import {
+  writeFile,
+  ExternalStorageDirectoryPath,
+  mkdir
+} from "react-native-fs";
 
-const DDP = DocumentDirectoryPath + "/";
+const DIR = mkdir(ExternalStorageDirectoryPath + "/ReactNative");
+const DDP = ExternalStorageDirectoryPath + "/ReactNative/";
+const output = str => str;
 const make_cols = refstr =>
   Array.from({ length: XLSX.utils.decode_range(refstr).e.c + 1 }, (x, i) =>
     XLSX.utils.encode_col(i)
@@ -15,17 +21,29 @@ export default class DownloadTest extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [[1, 2, 3], [4, 5, 6]],
-      widthArr: [60, 60, 60],
-      cols: make_cols("A1:C2")
+      data: [
+        ["Name", "Age", "Address"],
+        ["Bond", 35, "Tirunelveli"],
+        ["Edison", 15, "Tirunelveli"],
+        ["Vargess", 35, "Coimbatore"]
+      ],
+      hotelData: [
+        ["HotelName", "City"],
+        ["Marriot", "Coimbatore"],
+        ["LeMeriden", "Coimbatore"]
+      ],
+      widthArr: [60, 60, 60]
+      // cols: make_cols("A1:C2")
     };
     this.exportFile = this.exportFile.bind(this);
   }
 
   exportFile() {
     const ws = XLSX.utils.aoa_to_sheet(this.state.data);
+    const wt = XLSX.utils.aoa_to_sheet(this.state.hotelData);
     const wb = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, "sheetjs");
+    XLSX.utils.book_append_sheet(wb, wt, "sheetTwojs");
     const wbout = XLSX.write(wb, { type: "binary", bookType: "xlsx" });
     const file = DDP + "sheetjs.xlsx";
     writeFile(file, output(wbout), "ascii")
